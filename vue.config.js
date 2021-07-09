3f7bf0695b14d3e62b0f38e48616175b95780cb4
Viktor <malyshkin.viktor@icloud.com>
2021-07-09 18:03:16 +0300

    update rule webpack for img

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,34 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
+    transpileDependencies: [
+        'vuetify',
+    ],
 
-  runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production'
-      ? './'
-      : './',
-  outputDir: 'docs'
+    runtimeCompiler: true,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? './'
+        : './',
+    outputDir: 'docs',
+    //rules: [
+    //    {
+    //        test: /\.(png|jpg|gif)$/i,
+    //        use: [
+    //            {
+    //                loader: 'url-loader',
+    //                options: {
+    //                    limit: 8192,
+    //                },
+    //            },
+    //        ],
+    //    },
+    //],
+    chainWebpack: (config) => {
+      config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => {
+        options.fallback.options.name = "img/[name].[ext]"
+        return options
+      })
+    }
 }
